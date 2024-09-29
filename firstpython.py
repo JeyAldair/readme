a364fd9362ebdeb594292414069ce79adaedd9f2
JeyAldair <aldaircacerespalomino@gmail.com>
2024-09-29 04:40:46 -0500

    Update and rename firstpython.py to Data Science Languages

--- firstpython.py
+++
@@ -1,2 +0,0 @@
-
-En esta carpeta, encontrarás: El kit de apoyo, que incluye video tutoriales y guías diseñadas para facilitar la navegación dentro de la plataforma Coursera. Además, tendrás acceso a los únicos canales de atención para este curso.
